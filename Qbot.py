269bde71be65fe7c20ab5a994b14a2e97f8b57e6
__Underline__ <103819351+Underline-1024@users.noreply.github.com>
2025-06-26 08:22:01 +0800

    Update Qbot.py
    I have deleted my original key and appid.

--- Qbot.py
+++ Qbot.py
@@ -2,8 +2,9 @@ import botpy
 from botpy.types.message import Message
 from botpy.audio import Audio
 from model import Chat
-appid = "102090954"
-secret = "0aAkKuU4fGrS3eFrT5hJvXAnQ3gJwaEs"
+#The following code is applicable only to QQ robots.
+appid = "Your App ID"
+secret = "Your Secret Key"
 class MyClient(botpy.Client):
 
     async def on_at_message_create(self, message: Message):
@@ -11,4 +12,4 @@ class MyClient(botpy.Client):
         #await message.reply(content=f"机器人{self.robot.name}收到你的@消息了: {message.content}")
 intents = botpy.Intents(public_guild_messages=True, direct_message=True, guilds=True,audio_action=True)
 client = MyClient(intents=intents)
-client.run(appid="102090954", secret="0aAkKuU4fGrS3eFrT5hJvXAnQ3gJwaEs")
+client.run(appid=appid, secret=secret)
